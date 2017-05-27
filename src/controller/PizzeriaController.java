853f1eb8889cede0f50aa9531a2bb5d6ffb8ac0c
max2000warlord <max2000warlord@users.noreply.github.com>
2017-05-27 23:25:48 +1000

    Updated

--- src/controller/PizzeriaController.java
+++ src/controller/PizzeriaController.java
@@ -1,15 +1,25 @@
 package controller;
 
-import au.edu.uts.ap.javafx.Controller;
-import javafx.fxml.FXML;
-import model.Pizzeria;
+import au.edu.uts.ap.javafx.*;
+import javafx.event.*;
+import javafx.fxml.*;
+import javafx.stage.*;
+import javafx.scene.text.*;
+import javafx.scene.control.*;
+import model.*;
+import javafx.collections.*;
 
 public class PizzeriaController extends Controller<Pizzeria> {
 
     @FXML public void initialize() {
     }
 
-    public final Pizzeria getPizzeria() {
-        return model;
+    public final Pizzeria getPizzeria() {return model;}
+    
+    @FXML private ListView<Customer> custLv;
+    
+    private Customer getSelectedProduct(){
+    return custLv.getSelectionModel().getSelectedItem();
+    
     }
 }
