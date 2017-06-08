e368d9a35d03f9a190de9034f4a8e1a3185d14a3
max2000warlord <max2000warlord@users.noreply.github.com>
2017-06-08 10:10:09 +1000

    Update

--- src/controller/ReportController.java
+++ src/controller/ReportController.java
@@ -1,8 +1,25 @@
 package controller;
-
+import javafx.collections.*;
+import javafx.event.*;
+import javafx.fxml.*;
+import javafx.scene.text.*;
+import javafx.scene.control.*;
+import javafx.stage.*;
+import javafx.beans.binding.*;
+import javafx.beans.property.*;
+import java.io.*;
+import java.text.*;
+import java.lang.*;
+import model.*;
+import au.edu.uts.ap.javafx.*;
 import javafx.beans.binding.*;
 
-public class ReportController {
+public class ReportController extends Controller<Pizza> {
 
+public Pizza getPizza() {return model;}
+
+@FXML private void initialize() {}
+@FXML private Button closeWindow;
+@FXML private TableView reportTable;
 
 }
